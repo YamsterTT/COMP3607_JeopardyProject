2c0ababa89ca8c66f56bdb0f56896f6cd801ef39
HomieGab <isaiahrambhajan@gmail.com>
2025-11-27 09:49:24 -0400

    Player Test

---
+++ src/main/test/model/PlayerTest.java
@@ -0,0 +1,29 @@
+package edu.uwi.comp3607.model;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+
+    private Player player;
+
+    @BeforeEach
+    void setUp() {
+        player = new Player("p1", "Alice");
+    }
+
+    @Test
+    void testInitialization() {
+        assertEquals("p1", player.getId());
+        assertEquals("Alice", player.getName());
+        assertEquals(0, player.getScore());
+    }
+
+    @Test
+    void testAddScore() {
+        player.addScore(50);
+        assertEquals(50, player.getScore());
+        player.addScore(25);
+        assertEquals(75, player.getScore());
+    }
+}
